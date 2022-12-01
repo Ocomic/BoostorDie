56e7037ba4c8a5c9dc3e6ce8bfb811c4bbd155e3
Ocomic <108867917+Ocomic@users.noreply.github.com>
2022-12-01 13:47:45 +0100

    added movement
    - moevement cript for rocket
    - added obstacle

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    Rigidbody rbodyRocket;
+    [SerializeField] float mainThrust = 1000f;
+    [SerializeField] Vector3 roatation = Vector3.forward;
+    [SerializeField] float rotationSpeed = 100f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rbodyRocket = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        ProcessThrust();
+        ProcessRotation();
+    }
+    void ProcessThrust()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rbodyRocket.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            //Debug.Log("Space pressed");
+        }
         
+    }
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationSpeed);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        rbodyRocket.freezeRotation = true; //freez roatation so we can manually rotate
+        transform.Rotate(roatation * rotationThisFrame * Time.deltaTime);
+        rbodyRocket.freezeRotation = true; //unfreez roatation so physics system can take over
     }
 }
